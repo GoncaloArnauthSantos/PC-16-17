3c7255d97d4a5cd75baa2f615b82c2798d33a114
GoncaloArnauth <goncaloarnauth@outlook.com>
2017-05-26 17:05:17 +0100

    beggin of the project

---
+++ Serie1Pc/Serie1Pc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie1Pc
+{
+    public class ThrottledRegion
+    {
+        private object lockObj = new object();
+        private Dictionary<int, MyObject> map;
+        private int maxInside;
+        private int maxWaiting;
+        private int waitTimeout;
+
+        public ThrottledRegion(int maxInside, int maxWaiting, int waitTimeout)
+        {
+            map = new Dictionary<int, MyObject>();
+            this.maxInside = maxInside;
+            this.maxWaiting = maxWaiting;
+            this.waitTimeout = waitTimeout;
+        }
+
+        public bool TryEnter(int key)
+        {
+        }
+        public bool TryEnter(int key)
+        {
+        }
